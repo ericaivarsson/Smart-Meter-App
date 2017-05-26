f7556723d3602e5708c579eee78bcd55a176cb15
asoehler <arthursoehler@googlemail.com>
2017-05-26 10:00:14 +0200

    Added Comments and added "kWh" to function output

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 var txt;
 
+// Gibt Datum, Nutzerkennung und Verbrauchswert als String unter dem Display Button aus
 function displayText() {
     var x = document.forms[0];
     var nutz = x.elements[0].value;
     var wert = x.elements[1].value;
     var date = new Date().toISOString().slice(0, 10);
-   txt = "</br>" + date + ": " + "Nutzerkennung: " + nutz + "; " + "Verbrauchswert: " + wert;
+   txt = "</br>" + date + ": " + "Nutzerkennung: " + nutz + "; " + "Verbrauchswert: " + wert + " kWh";
     document.getElementById("output").innerHTML += txt;
 }
 
-
+// Funktion um zufällig generierte Stromstärke und Spannung zu erhalten, 
+// die eine Warnung ausgibt sobald die Stromspannung das Maximum überschreitet.
 function randomizeStuff(){
     var spannung = document.getElementById("spannung");
     spannung.innerHTML = ((Math.random() * 21 + 220).toFixed(1));
